8d4c785a5f892021b2877561b0752a57bb5aa53e
Kris Coleman <kris@blacksuitmedia.com>
2016-09-04 05:15:44 -0400

    nuget...

--- System.InternetTime/System.InternetTime/Build.bat
+++ System.InternetTime/System.InternetTime/Build.bat
@@ -14,10 +14,10 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\System.InternetTime\System.InternetTime.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\System.InternetTime.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net40
 
-%nuget% pack "src\System.InternetTime/System.InternetTime/System.InternetTime/System.InternetTime.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "src\System.InternetTime.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
