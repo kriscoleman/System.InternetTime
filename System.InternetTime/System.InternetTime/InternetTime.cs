03d03bf319a12e23bdf2ab5c55e9ca6cc8029518
Kris Coleman <kris@blacksuitmedia.com>
2016-09-04 03:50:17 -0400

    Initial Checkin

---
+++ System.InternetTime/System.InternetTime/InternetTime.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ModernHttpClient;
+
+namespace System.InternetTime
+{
+    public class InternetTime
+    {
+        public const string NistUrl = "http://nist.time.gov/actualtime.cgi?lzbc=siqm9b";
+        const string MediaTypeHeaderValue = "text/html, application/xhtml+xml, */*";
+
+
+
+        public static async Task<DateTime?> GetNistTimeAsync()
+        {
+            var client = new HttpClient(new NativeMessageHandler {DisableCaching = true});
+            client.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse(MediaTypeHeaderValue));
+            var response = await client.GetAsync(NistUrl);
+            if (!response.IsSuccessStatusCode) return null;
+
+            var stream = await response.Content.ReadAsStreamAsync();
+            var streamReader = new StreamReader(stream);
+            var content = await streamReader.ReadToEndAsync();
+            var time = Regex.Match(content, @"(?<=\btime="")[^""]*").Value; //<timestamp time=\"1395772696469995\" delay=\"1395772696469995\"/>
+            var milliseconds = Convert.ToInt64(time) / 1000.0;
+
+            DateTime? dateTime = new DateTime(1970, 1, 1).AddMilliseconds(milliseconds).ToLocalTime();
+            return dateTime;
+        }
+
+        public static DateTime? GetNistTime() => GetNistTimeAsync().Result;
+    }
+}
---
+++ System.InternetTime/Tests/GetNistTimeTests.cs
@@ -0,0 +1,25 @@
+﻿using System.InternetTime;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class GetNistTimeTests
+    {
+
+        [Test]
+        public async Task GetNistTimeAsync()
+        {
+            var time = await InternetTime.GetNistTimeAsync();
+            Assert.That(time != null, "Could not reach server");
+        }
+
+        [Test]
+        public void GetNistTime()
+        {
+            var time = InternetTime.GetNistTime();
+            Assert.That(time != null, "Could not reach server");
+        }
+    }
+}
