dca633ae10f8a423dfcb46e9d7b7c8b2751cb113
Kris Coleman <kris@blacksuitmedia.com>
2016-09-04 20:04:46 -0400

    Some changes to Client for Faking

--- System.InternetTime/System.InternetTime/Client.cs
+++ System.InternetTime/System.InternetTime/Client.cs
@@ -3,16 +3,13 @@ using System.Threading.Tasks;
 namespace System.InternetTime
 {
     /// <summary>
-    /// This is a agnostic Static Helper class to be used by IInternetTime implentations. Ensures they share some core functionality and aids in faking.
+    ///     This is a agnostic Static Helper class to be used by IInternetTime implentations. Ensures they share some core
+    ///     functionality and aids in faking.
     /// </summary>
     public class Client
     {
-        readonly string _url;
-        readonly string _mediaTypeHeaderValue;
-        readonly Func<string, double> _responseToMillisecondsFunc;
-
         /// <summary>
-        /// Gets time asynchronously. Can be awaited to avoid blocking UI due to latency. 
+        ///     Gets time asynchronously. Can be awaited to avoid blocking UI due to latency.
         /// </summary>
         /// <param name="url">The URL.</param>
         /// <param name="mediaTypeHeaderValue">The media type header value.</param>
@@ -21,35 +18,41 @@ namespace System.InternetTime
         public Client(string url, string mediaTypeHeaderValue,
             Func<string, double> responseToMillisecondsFunc)
         {
-            _url = url;
-            _mediaTypeHeaderValue = mediaTypeHeaderValue;
-            _responseToMillisecondsFunc = responseToMillisecondsFunc;
+            Url = url;
+            MediaTypeHeaderValue = mediaTypeHeaderValue;
+            ResponseToMillisecondsFunc = responseToMillisecondsFunc;
         }
 
+        public virtual Func<string, double> ResponseToMillisecondsFunc { get; }
+
+        public virtual string Url { get; }
+        public virtual string MediaTypeHeaderValue { get; }
+
         /// <summary>
-        /// Gets time asynchronously. Can be awaited to avoid blocking UI due to latency. 
+        ///     Gets time asynchronously. Can be awaited to avoid blocking UI due to latency.
         /// </summary>
         /// <returns>The NIST time, in the default time zone</returns>
         public virtual async Task<DateTime?> GetAsync()
         {
-            var content = await SimpleHttpClient.GetAsync(_url, _mediaTypeHeaderValue);
+            var content = await SimpleHttpClient.GetAsync(Url, MediaTypeHeaderValue);
             if (content == null) return null; //couldn't reach server
 
-            var milliseconds = _responseToMillisecondsFunc(content);
+            var milliseconds = ResponseToMillisecondsFunc(content);
             var dateTime = ConvertMillisecondsToDateTime(milliseconds);
             return dateTime;
         }
 
         /// <summary>
-        /// Gets time. Not asynchronous so may block UI due to latency. 
+        ///     Gets time. Not asynchronous so may block UI due to latency.
         /// </summary>
         /// <returns>The NIST time, in the default time zone</returns>
         public DateTime? Get() => GetAsync().Result;
 
         /// <summary>
-        /// Converts the milliseconds to date time.
+        ///     Converts the milliseconds to date time.
         /// </summary>
         /// <param name="milliseconds">The milliseconds.</param>
-        public virtual DateTime ConvertMillisecondsToDateTime(double milliseconds) => new DateTime(1970, 1, 1).AddMilliseconds(milliseconds);
+        public virtual DateTime ConvertMillisecondsToDateTime(double milliseconds)
+            => new DateTime(1970, 1, 1).AddMilliseconds(milliseconds);
     }
 }
